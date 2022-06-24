0a46798a7bb3c398be5f06cffaad34758ec6fcc7
Suha Siddiqui <suhasid@my.yorku.ca>
2022-06-24 10:56:11 -0400

    functioning text stat example with hard coded multi-line string

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 import textstat  # https://pypi.org/project/textstat/
-import os
-import glob       # using to read in many files
-import docx2txt   # because I'm reading in word docs
-# where is the folder with your content?
-folderPath = 'C:\\Users\suhas\git\text-stat-example\folder'
-# I want to search in all of the folders, so I set recursive=True
-docs=[]
-docs = glob.glob(folderPath + '/**/*.txt',recursive=True)
-docs.extend(glob.glob(folderPath + '/**/*.docx',recursive=True))
-#... keep adding whatever types you need
-print(docs)
-# the language is English by default so no need to set the language
-# Loop through my docs
-for doc in docs:
-    if os.path.isfile(doc):
-        text = docx2txt.process(os.path.join(folderPath,doc))
-        
-        print('Document:                     ' + doc)
-        print('Flesch Reading Ease:          ' + str(textstat.flesch_reading_ease(text)))
-        print('Smog Index:                   ' + str(textstat.smog_index(text)))
-        print('Flesch Kincaid Grade:         ' + str(textstat.flesch_kincaid_grade(text)))
-        print('Coleman Liau Index:           ' + str(textstat.coleman_liau_index(text)))
-        print('Automated Readability Index:  ' + str(textstat.automated_readability_index(text)))
-        print('Dale Chall Readability Score: ' + str(textstat.dale_chall_readability_score(text)))
-        print('Difficult Words:              ' + str(textstat.difficult_words(text)))
-        print('Linsear Write Formula:        ' + str(textstat.linsear_write_formula(text)))
-        print('Gunning Fog:                  ' + str(textstat.gunning_fog(text)))
-        print('Text Standard:                ' + str(textstat.text_standard(text)))
-        print('*********************************************************************************')
+
+text=("This report will describe current issues facing light pollution and potential solutions"
+ "dealing with the UN sustainable goal that “ensures access to affordable, reliable, sustainable"
+ "and modern energy for all” [18]. The potential solutions will not only describe the cutting-edge"
+ "technology that Electrip Inc. provides but will also include other solutions that can also improve"
+ "the electricity consumption through light usage. The document will provide research that will help"
+ "convince the user to save electricity by describing the feasibility of doing so. The report will"
+ "cover the human involvement and environmental (human health, conditions, corporate and personal"
+ "financial significance, atmospheric impact, ecosystems, migration pattern, wildlife life cycles)"
+ "involvement in light pollution and excessive consumption. Furthermore, other topics such as simple"
+ "solutions that can be achieved form personal initiative (Psychological techniques and energy"
+ "efficient light investments), along side smart technology that Electrip Inc. has to offer."
+ "Additionally, the mechanism will be described in terms of logistics and technology involved"
+ "(Sensor/actuator interface with AI). The scope of the report will mainly cover the need for"
+ "Electrip Inc. the personal/home environment as well as workspaces and technicality of the"
+ "sensor/actuator interface and how the AI will interact with the currently integrated technology"
+ "present in current spaces. It will explore ultra wideband wireless connection, frequency and"
+ "concepts of data domains utilized by the AI. The document will not cover the installation"
+ "processes and the specific costs for technology integration as technology integration methodology"
+ "will vary across different environments due to infrastructure of the building’s circuitry.")
+
+print(textstat.flesch_reading_ease(text))
+print(textstat.flesch_kincaid_grade(text))
+print('*********************************************************************************')
+
 """Flesch-Kincaid Grade Level = This is a grade formula in that a score of 9.3 means that a ninth grader would be able to read the document"""
 """Gunning Fog = This is a grade formula in that a score of 9.3 means that a ninth grader would be able to read the document."""
 """SMOG - for 30 sentences or more  =This is a grade formula in that a score of 9.3 means that a ninth grader would be able to read the document."""
